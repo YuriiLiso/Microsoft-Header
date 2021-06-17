63567cf08c37b1129204fe3253399d51237b31d9
Yurii Lisovenko <yriyliso@gmail.com>
2021-06-17 08:04:13 +0300

    Updates

---
+++ precache-manifest.4639e8206e02b04cd73e92c4e21cbead.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962997ed788353def5744e2fd63213d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f722e369177878aa13b3",
+    "url": "./static/css/main.c5fbfa34.chunk.css"
+  },
+  {
+    "revision": "7e2390130278bf593e77",
+    "url": "./static/js/2.18157275.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18157275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f722e369177878aa13b3",
+    "url": "./static/js/main.9c7538ba.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  },
+  {
+    "revision": "e0dd270686c43937cb5819cd2ff7bd3b",
+    "url": "./static/media/microsoft-logo.e0dd2706.png"
+  },
+  {
+    "revision": "27e752386aaccc32b18847c7679811c7",
+    "url": "./static/media/telega.27e75238.png"
+  }
+]);
